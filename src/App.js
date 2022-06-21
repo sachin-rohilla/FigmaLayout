acb5631aa1e79e3460542ce845f6ebe154376be8
sachin-rohilla <sachinrohilla23799@gmail.com>
2022-06-21 11:54:01 +0530

    FigmaLayout

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import Appointment from "./Components2/Appointment";
+import Details from "./Components2/Details";
+import Navbar from "./Components2/Navbar";
+import Section from "./Components2/Section";
+import Footer from "./Components2/Footer";
+function App() {
+  return (
+    <>
+      <Navbar />
+      <Section />
+      <Appointment />
+      <Details />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
