a2bdc0ee20b8f72af2a0da11801a8fe615ba2f4b
sachin-rohilla <sachinrohilla23799@gmail.com>
2022-06-24 11:41:03 +0530

    FigmaLayout11

--- src/Components4/Details.js
+++ src/Components4/Details.js
@@ -1,68 +1,48 @@
 import React from "react";
 import "./Details.css";
+
+import Data from "./Data.json";
 const Details = () => {
   return (
     <>
       <h2 className="main-heading">Select Clinic</h2>
-      <div className="details-container">
-        <div className="main">
-          <div>
-            <img
-              src="https://previews.123rf.com/images/lithian/lithian1602/lithian160200034/52548478-portrait-of-an-attractive-young-female-doctor-in-white-coat-.jpg"
-              width="100px"
-              alt="doctor"
-            />
-          </div>
-          <div>
-            <p className="para1">Orthopeadic</p>
-            <p className="para2">Positive Ortho Clinic</p>
-            <p className="para3">Kullapatty, Hyderabad</p>
-            <p className="para4">MON-FRI</p>
-            <p className="para5">7:30AM-8:00PM</p>
-          </div>
-          <div>
-            <button className="btn1">Book Now</button>
-          </div>
-        </div>
-        <div className="main">
-          <div>
-            <img
-              src="https://previews.123rf.com/images/lithian/lithian1602/lithian160200034/52548478-portrait-of-an-attractive-young-female-doctor-in-white-coat-.jpg"
-              width="100px"
-              alt="doctor"
-            />
-          </div>
-          <div>
-            <p className="para1">Orthopeadic</p>
-            <p className="para2">Positive Ortho Clinic</p>
-            <p className="para3">Kullapatty, Hyderabad</p>
-            <p className="para4">MON-FRI</p>
-            <p className="para5">7:30AM-8:00PM</p>
-          </div>
-          <div>
-            <button className="btn2">Book Now</button>
-          </div>
-        </div>
-        <div className="main">
-          <div>
-            <img
-              src="https://previews.123rf.com/images/lithian/lithian1602/lithian160200034/52548478-portrait-of-an-attractive-young-female-doctor-in-white-coat-.jpg"
-              width="100px"
-              alt="doctor"
-            />
-          </div>
-          <div>
-            <p className="para1">Orthopeadic</p>
-            <p className="para2">Positive Ortho Clinic</p>
-            <p className="para3">Kullapatty, Hyderabad</p>
-            <p className="para4">MON-FRI</p>
-            <p className="para5">7:30AM-8:00PM</p>
-          </div>
-          <div>
-            <button className="btn3">Book Now</button>
-          </div>
-        </div>
-      </div>
+
+      {Data.map((item) => {
+        return (
+          <>
+            <div className="details-container">
+              <div className="main">
+                <div>
+                  <img src={item.image} width="100px" alt="doctor" />
+                </div>
+                <div>
+                  <p className="para1">{item.heading}</p>
+                  <p className="para2">{item.name}</p>
+                  <p className="para3">{item.title}</p>
+                  <p className="para4">{item.experirence}</p>
+                </div>
+                <div>
+                  <button className="btn1">{item.button}</button>
+                </div>
+              </div>
+              <div className="main">
+                <div>
+                  <img src={item.image} width="100px" alt="doctor" />
+                </div>
+                <div>
+                  <p className="para1">{item.heading}</p>
+                  <p className="para2">{item.name}</p>
+                  <p className="para3">{item.title}</p>
+                  <p className="para4">{item.experirence}</p>
+                </div>
+                <div>
+                  <button className="btn1">{item.button}</button>
+                </div>
+              </div>
+            </div>
+          </>
+        );
+      })}
     </>
   );
 };
