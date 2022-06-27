da6edf72df54f9704323d4264b4d28f30258c121
sachin-rohilla <sachinrohilla23799@gmail.com>
2022-06-27 09:23:16 +0530

    figma11

--- src/App.js
+++ src/App.js
@@ -4,12 +4,14 @@ import Details from "./Components4/Details";
 import Footer from "./Components4/Footer";
 import Navbar from "./Components4/Navbar";
 import SectionList from "./Components4/SectionList";
+
 function App() {
   return (
     <>
       <Navbar />
       <Profile />
       <Details />
+      
       <SectionList />
       <Footer />
     </>
--- src/Components4/Details.js
+++ src/Components4/Details.js
@@ -1,73 +1,71 @@
-import React from "react";
 import "./Details.css";
 
 import Data from "./Data.json";
+import { useState } from "react";
 const Details = () => {
   const button = "Book Now";
+  const [filter, setFiler] = useState("");
+  const searchContent = (e) => {
+    setFiler(e.target.value);
+  };
+  let searchData = Data.filter((item) => {
+    return Object.keys(item).some((key) =>
+      item[key]
+        .toString()
+        .toUpperCase()
+        .includes(filter.toString().toUpperCase())
+    );
+  });
+
   return (
     <>
-      <h2 className="main-heading">Select Clinic</h2>
+      <div className="input-container">
+        <input
+          className="input"
+          type="text"
+          placeholder="Search here"
+          value={filter}
+          onChange={searchContent.bind(this)}
+        />
+        <button className="input-button"> Search </button>
+      </div>
 
-      {Data.map((item) => {
-        return (
-          <>
-            <div className="details-container">
-              <div className="main">
-                <div>
-                  <img src={item.image} width="100px" alt="doctor" />
-                </div>
-                <div>
-                  <p className="para1">{item.heading}</p>
-                  <p className="para2">{item.name}</p>
-                  <p className="para3">{item.title}</p>
-                  <span class={item.star}></span>
+      <div className="flex-container">
+        {searchData.map((item) => {
+          return (
+            <>
+              <div className="details-container" key={item.id}>
+                <div className="main">
+                  <div>
+                    <img src={item.image} width="100px" alt="doctor" />
+                  </div>
+                  <div>
+                    <p className="para1">{item.heading}</p>
+                    <p className="para2">{item.name}</p>
+                    {new Array(parseInt(item.star)).fill(0).map((value) => {
+                      return <span class="fa fa-star"></span>;
+                    })}
 
-                  <span
-                    style={{
-                      marginTop: "10px",
-                      fontSize: "12px",
-                      color: "#58585B",
-                    }}
-                  >
-                    {item.reivew}
-                  </span>
-                  <p className="para4">{item.experirence}</p>
-                </div>
-                <div>
-                  <button className="btn1">{button}</button>
+                    <p className="para3">{item.title}</p>
+
+                    <span
+                      style={{
+                        marginTop: "10px",
+                        fontSize: "12px",
+                        color: "#58585B",
+                      }}
+                    ></span>
+                    <p className="para4">{item.experirence}</p>
+                  </div>
+                  <div>
+                    <button className="btn1">{button}</button>
+                  </div>
                 </div>
               </div>
-              <div className="main">
-                <div>
-                  <img src={item.image} width="100px" alt="doctor" />
-                </div>
-                <div>
-                  <p className="para1">{item.heading}</p>
-                  <p className="para2">{item.name}</p>
-                  <p className="para3">{item.title}</p>
-                  <span class="fa fa-star checked"></span>
-                  <span class="fa fa-star checked"></span>
-                  <span class="fa fa-star checked"></span>
-                  <span class="fa fa-star checked"></span>
-                  <span
-                    style={{
-                      marginTop: "10px",
-                      fontSize: "12px",
-                      color: "#58585B",
-                    }}
-                  >
-                    {item.reivew}
-                  </span>
-                  <p className="para4">{item.experirence}</p>
-                </div>
-                <div>
-                  <button className="btn1">{button}</button>
-                </div>
-              </div>
-            </div>
-          </>
-        );
-      })}
+            </>
+          );
+        })}
+      </div>
     </>
   );
 };
