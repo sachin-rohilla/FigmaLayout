67afa6fe83b52b5211c38ff964cce778b7638536
sachin-rohilla <sachinrohilla23799@gmail.com>
2022-06-30 18:10:36 +0530

    AddFaq

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
-
 import Navbar from "./Components5/Navbar";
 import Image from "./Components5/Image";
 import Section from "./Components5/Section";
 import SectionList from "./Components5/SectionList";
 import Footer from "./Components5/Footer";
+import RecentBlogs from "./Components5/RecentBlogs";
+import Faq from "./Components5/Faq";
 function App() {
   return (
     <>
       <Navbar />
-      <Image/>
-      <Section/>
-      <SectionList/>
-      <Footer/>
-    
+      <Image />
+      <Section />
+      <Faq />
+      <RecentBlogs />
+      <SectionList />
+      <Footer />
     </>
   );
 }
