568390ee59d56f4ba0969d4b2c234b0cc9c4e7c1
sachin-rohilla <sachinrohilla23799@gmail.com>
2022-07-05 11:04:25 +0530

    stopwatch

--- src/App.js
+++ src/App.js
@@ -1,32 +1,9 @@
-// import Navbar from "./Components5/Navbar";
-// import Image from "./Components5/Image";
-// import Section from "./Components5/Section";
-// import SectionList from "./Components5/SectionList";
-// import Footer from "./Components5/Footer";
-// import RecentBlogs from "./Components5/RecentBlogs";
-// import Faq from "./Components5/Faq";
-
-
-import Stopwatch from "./Components5/Stopwatch/Stopwatch";
-
-
+import Stopwatch from "./Stopwatch/Stopwatch";
 
 function App() {
-
   return (
     <>
-      {/* <Navbar />
-      <Image />
-      <Section />
-      <Faq />
-      <RecentBlogs />
-      <SectionList />
-      <Footer /> */}
-      <Stopwatch/>
-      
-      
-      
-
+      <Stopwatch />
     </>
   );
 }
